6690f2e31e6b3593d2709575f0052662272e9452
Kellibledsoe <kellibledsoe123@gmail.com>
2024-09-05 19:43:44 -0500

    homework 19
    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+console.log("Your index.js file is loaded correctly!");
+
+//$("#myID").on("click", function(){
+   //console.log("You clicked on #myID"); 
+//}); 
+
+
+
+//$("#myID").on("click", function(){
+ //  $(".fadeMe").fadeToggle("slow");
+//});
+
+//function printMessage() {
+  // document.getElementById('output')innerHTML = "Welcome";
+//}
+$( "p" ).trigger( "click" );
